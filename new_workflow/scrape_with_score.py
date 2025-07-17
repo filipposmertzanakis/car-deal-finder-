c7ae84d4ac5341f3fa7f1d408eb06de9b56db3d1
Φιλιππος Μερτζανακης <97954452+filipposmertzanakis@users.noreply.github.com>
2025-07-17 10:42:37 +0300

    Update scrape_with_score.py

--- new_workflow/scrape_with_score.py
+++ new_workflow/scrape_with_score.py
@@ -95,6 +95,7 @@ def get_driver():
     options = uc.ChromeOptions()
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
+    options.add_argument("--headless")
     options.add_argument("window-size=1920,1080")
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.6367.207 Safari/537.36")
     
